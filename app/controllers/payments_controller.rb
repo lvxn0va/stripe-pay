7c3b46587bd565c9e51cb36206337b0c7d1c4ef2
Jordan Stephens <iam@jordanstephens.net>
2012-02-01 20:22:16 -0600

    removed #edit, #update, #show and #destroy from payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,6 @@
 class PaymentsController < ApplicationController
   
   def thankyou
-    
   end
   
   # GET /payments
@@ -10,15 +9,6 @@ class PaymentsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-    end
-  end
-
-  # GET /payments/1
-  def show
-    @payment = Payment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
     end
   end
 
@@ -31,11 +21,6 @@ class PaymentsController < ApplicationController
     end
   end
 
-  # GET /payments/1/edit
-  def edit
-    @payment = Payment.find(params[:id])
-  end
-
   # POST /payments
   def create
     @payment = Payment.new(params[:payment])
@@ -45,30 +30,6 @@ class PaymentsController < ApplicationController
       else
         format.html { render action: "new" }
       end
-    end
-  end
-
-  # PUT /payments/1
-  def update
-    @payment = Payment.find(params[:id])
-
-    respond_to do |format|
-      if @payment.update_attributes(params[:payment])
-        format.html { redirect_to @payment, notice: 'Payment was successfully updated.' }
-      else
-        format.html { render action: "edit" }
-      end
-    end
-  end
-
-  # DELETE /payments/1
-  # DELETE /payments/1.json
-  def destroy
-    @payment = Payment.find(params[:id])
-    @payment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to payments_url }
     end
   end
 end
